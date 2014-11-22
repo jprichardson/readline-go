9813fd3302ac95f6c1d04e2b7e84780f3747feac
Ben Allen <bsallen@alcf.anl.gov>
2014-11-22 17:20:07 -0600

    Fix and test for "panic: runtime error: index out of range for" lines that contain just a \n

--- readline.go
+++ readline.go
@@ -1,9 +1,9 @@
 package readline
 
 import (
-	"io"
 	"bufio"
 	"bytes"
+	"io"
 )
 
 func ReadLine(reader io.Reader, f func(string)) {
@@ -11,10 +11,10 @@ func ReadLine(reader io.Reader, f func(string)) {
 	line, err := buf.ReadBytes('\n')
 	for err == nil {
 		line = bytes.TrimRight(line, "\n")
-		if line[len(line)-1] == 13 { //'\r'
-			line = bytes.TrimRight(line, "\r")
-		}
 		if len(line) > 0 {
+			if line[len(line)-1] == 13 { //'\r'
+				line = bytes.TrimRight(line, "\r")
+			}
 			f(string(line))
 		}
 		line, err = buf.ReadBytes('\n')
